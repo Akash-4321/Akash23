3a25ea371b73ebdca6651260ffaee047a58d34bb
Akash-4321 <117497088+Akash-4321@users.noreply.github.com>
2023-09-25 02:12:45 +0530

    Add files via upload
    Backend_Node

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mysql = require("mysql2");
+const cors = require("cors");
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "Akash123@",
+  database: "ems",
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/api/get", (req, res) => {
+  const sqlGet = "SELECT * FROM project";
+  db.query(sqlGet, (error, result) => {
+    if (error) {
+      console.error("Error executing SQL query:", error);
+      res.status(500).json({ error: "Internal Server Error", details: error.message });
+      return;
+    }
+    res.send(result);
+  });
+});
+
+app.get('/api/search', (req, res) => {
+  const { keyword } = req.query;
+
+  // Use placeholders and parameterized queries to prevent SQL injection
+  const sqlSearch = "SELECT * FROM project WHERE `Project_Title` LIKE ?";
+  const searchKeyword = '%' + keyword + '%';
+
+  db.query(sqlSearch, [searchKeyword], (error, result) => {
+    if (error) {
+      console.error('Error executing SQL query:', error);
+      res.status(500).json({ error: 'Internal Server Error', details: error.message });
+      return;
+    }
+    res.send(result);
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
+
